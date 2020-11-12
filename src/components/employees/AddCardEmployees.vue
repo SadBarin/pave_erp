59728bd608c175a45d5e9a384b4a5d66fec8e922
SadBarin <sadbarin@yandex.com>
2020-11-12 22:57:25 +0300

    add validation for addCard form and addEmployee form

--- src/components/employees/AddCardEmployees.vue
+++ src/components/employees/AddCardEmployees.vue
@@ -1,8 +1,23 @@
 <template>
-  <form @submit.prevent="onSubmit" class="button-container" >
+  <form @submit.prevent="submitEmployee" class="button-container" >
     <div class="input-field input-field-blue">
-      <input type="text" id="name" v-model="name">
+      <input type="text"
+             id="name"
+             v-model.trim="name"
+             :class="{invalid: ($v.name.$dirty && !$v.name.required) || ($v.name.$dirty && !$v.name.minLength)}">
       <label for="name">Имя</label>
+      <small
+        class="helper-text invalid"
+        v-if="$v.name.$dirty && !$v.name.required"
+      >
+        Введите имя
+      </small>
+      <small
+        class="helper-text invalid"
+        v-else-if="$v.name.$dirty && !$v.name.minLength"
+      >
+        Имя должно содержать не менее {{$v.name.$params.minLength.min}} символов.
+      </small>
     </div>
 
     <button class="btn-flat waves-effect waves-light auth-submit blue darken-1 white-text" type="submit">Добавить сотрудника</button>
@@ -10,6 +25,8 @@
 </template>
 
 <script>
+import { required, minLength } from 'vuelidate/lib/validators'
+
 export default {
   name: 'AddCardEmployees',
   data () {
@@ -17,8 +34,16 @@ export default {
       name: ''
     }
   },
+  validations: {
+    name: { required, minLength: minLength(2) }
+  },
   methods: {
-    onSubmit () {
+    submitEmployee () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
       if (this.name.trim()) {
         const newEmployee = {
           id: Date.now(),
--- src/components/sites/AddCardSites.vue
+++ src/components/sites/AddCardSites.vue
@@ -1,8 +1,24 @@
 <template>
-  <form @submit.prevent="onSubmit" class="button-container" >
+  <form @submit.prevent="submitSites" class="button-container" >
     <div class="input-field input-field-blue">
-      <input type="text" id="city" v-model="cityName">
+      <input type="text"
+             id="city"
+             v-model.trim="cityName"
+             :class="{invalid: ($v.cityName.$dirty && !$v.cityName.required) || ($v.cityName.$dirty && !$v.cityName.minLength)}"
+      >
       <label for="city">Город</label>
+      <small
+        class="helper-text invalid"
+        v-if="$v.cityName.$dirty && !$v.cityName.required"
+      >
+        Введите город
+      </small>
+      <small
+        class="helper-text invalid"
+        v-else-if="$v.cityName.$dirty && !$v.cityName.minLength"
+      >
+        Город должен содержать не менее {{$v.cityName.$params.minLength.min}} символов.
+      </small>
     </div>
 
     <button class="btn-flat waves-effect waves-light auth-submit blue darken-1 white-text" type="submit">Добавить город</button>
@@ -10,6 +26,8 @@
 </template>
 
 <script>
+import { required, minLength } from 'vuelidate/lib/validators'
+
 export default {
   name: 'AddCardSites',
   data () {
@@ -17,8 +35,16 @@ export default {
       cityName: ''
     }
   },
+  validations: {
+    cityName: { required, minLength: minLength(2) }
+  },
   methods: {
-    onSubmit () {
+    submitSites () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
       if (this.cityName.trim()) {
         const newCity = {
           id: Date.now(),
