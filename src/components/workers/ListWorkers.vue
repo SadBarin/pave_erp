5664421f3a88569b33431e4c3af8fe8842b2faae
SadBarin <SadBarin@yandex.by>
2021-01-13 15:31:28 +0300

    Workers Cleaning

--- src/components/workers/ListWorkers.vue
+++ src/components/workers/ListWorkers.vue
@@ -1,22 +1,67 @@
 <template>
   <section class="workers_section">
-    <CardWorkers
-      v-for="worker of workers"
-      :key="worker.id"
-      v-bind:worker="worker"
-      v-on:remove-worker="removeWorker"
-    />
+    <table>
+      <tr>
+        <th>Фамилия</th>
+        <th>Имя</th>
+        <th>Отчество</th>
+        <th>Пол</th>
+        <th>Возраст</th>
+        <th>Мед.книжка</th>
+        <th>Город</th>
+        <th>Телефон</th>
+        <th>Профессия</th>
+        <th></th>
+      </tr>
+      <tr v-for="worker in workers" :key="worker">
+        <td>{{worker.surname}}</td>
+        <td>{{worker.name}}</td>
+        <td>{{worker.patronymic}}</td>
+        <td>{{worker.sex}}</td>
+        <td>{{worker.age}}</td>
+        <td>{{worker.medicalBook}}</td>
+        <td>{{worker.city}}</td>
+        <td>{{worker.mobilePhone}}</td>
+        <td>{{worker.professions}}</td>
+        <td>
+          <div class="flex-center">
+            <button class="btn-transparent btn-remove transparent waves-effect waves-light auth-submit blue-text text-darken-1"
+                    title="Просмотреть"
+            >
+              <i class="material-icons">remove_red_eye</i>
+            </button>
+
+            <button class="btn-transparent btn-remove transparent waves-effect waves-light auth-submit blue-text text-darken-1"
+                    title="Удалить"
+                    v-if="!worker.edited"
+                    v-on:click="popupVisibility(worker.id)"
+            >
+              <i class="material-icons">delete</i>
+            </button>
+
+            <button class="btn-transparent transparent waves-effect auth-submit blue-text text-darken-1"
+                    title="Редактировать"
+                    v-on:click="editedWorkerStatus(worker.id)"
+            >
+              <i class="material-icons">create</i>
+            </button>
+
+          </div>
+        </td>
+      </tr>
+    </table>
   </section>
 </template>
 
 <script>
-import CardWorkers from '@/components/workers/CardWorkers'
 export default {
   name: 'ListWorkers',
-  components: { CardWorkers },
   methods: {
-    removeWorker (id) {
-      this.$emit('remove-worker', id)
+    popupVisibility (id) {
+      this.$emit('popup-visibility', id)
+    },
+    editedWorkerStatus (id) {
+      this.$emit('edited-worker-status', id)
     }
   },
   props: ['workers']
@@ -24,7 +69,4 @@ export default {
 </script>
 
 <style scoped>
-  .row {
-    margin-bottom: 0;
-  }
 </style>
--- src/views/Workers.vue
+++ src/views/Workers.vue
@@ -30,76 +30,27 @@
     </div>
 
     <section>
-<!--      <ListWorkers-->
-<!--        v-if="workers.length"-->
-<!--        v-bind:workers="workers"-->
-<!--        @remove-worker="removeWorker"-->
-<!--      />-->
-<!--      <div v-else class="empty-list">-->
-<!--        <h5 class="empty-list-title"><i class="material-icons">mood_bad</i> Рабочих не осталось!</h5>-->
-<!--        <p>Добавьте рабочего, чтобы начать работать над ним.</p>-->
-<!--      </div>-->
-
-      <table>
-        <tr>
-          <th>Фамилия</th>
-          <th>Имя</th>
-          <th>Отчество</th>
-          <th>Пол</th>
-          <th>Возраст</th>
-          <th>Мед.книжка</th>
-          <th>Город</th>
-          <th>Телефон</th>
-          <th>Профессия</th>
-          <th></th>
-        </tr>
-        <tr v-for="worker in workers" :key="worker">
-          <td>{{worker.surname}}</td>
-          <td>{{worker.name}}</td>
-          <td>{{worker.patronymic}}</td>
-          <td>{{worker.sex}}</td>
-          <td>{{worker.age}}</td>
-          <td>{{worker.medicalBook}}</td>
-          <td>{{worker.city}}</td>
-          <td>{{worker.mobilePhone}}</td>
-          <td>{{worker.professions}}</td>
-          <td>
-            <div class="flex-center">
-              <button class="btn-transparent transparent waves-effect auth-submit blue-text text-darken-1"
-                      title="Подробнее"
-              >
-                <i class="material-icons">remove_red_eye</i>
-              </button>
-
-              <button class="btn-transparent btn-remove transparent waves-effect waves-light auth-submit blue-text text-darken-1"
-                      title="Удалить"
-                      v-if="!worker.edited"
-                      v-on:click="popupVisibility(worker.id)"
-              >
-                <i class="material-icons">delete</i>
-              </button>
-
-              <button class="btn-transparent transparent waves-effect auth-submit blue-text text-darken-1"
-                      title="Редактировать"
-                      v-on:click="editedWorkerStatus(worker.id)"
-              >
-                <i class="material-icons">create</i>
-              </button>
-
-            </div>
-          </td>
-        </tr>
-      </table>
+      <ListWorkers
+        v-if="workers.length"
+        v-bind:workers="workers"
+        @popup-visibility="popupVisibility"
+        @edited-worker-status="editedWorkerStatus"
+      />
+      <div v-else class="empty-list">
+        <h5 class="empty-list-title"><i class="material-icons">mood_bad</i> Рабочих не осталось!</h5>
+        <p>Добавьте рабочего, чтобы начать работать над ним.</p>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
 import AddCardWorkers from '@/components/workers/AddCardWorkers'
+import ListWorkers from '@/components/workers/ListWorkers'
 import Popup from '@/components/Popup'
 export default {
   name: 'Workers',
-  components: { AddCardWorkers, Popup },
+  components: { AddCardWorkers, ListWorkers, Popup },
   data () {
     return {
       workers: [],
